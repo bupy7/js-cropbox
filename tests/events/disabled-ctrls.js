ff324d7c000f4378697113561e1bbe5bd49c461c
Vasilij Belosludcev <bupy765@gmail.com>
2017-04-29 20:19:06 +0500

    #3

---
+++ tests/events/disabled-ctrls.js
@@ -0,0 +1,9 @@
+describe('onCbEnabledCtrls', function() {
+    it('should trigger', function(done) {
+        var cropbox = new Cropbox('#plugin');
+        cropbox.getCb().addEventListener('cb.enabledCtrls', function() {
+            done();
+        });
+        cropbox.load('assets/image.png');
+    });
+});
