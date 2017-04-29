3762154330c180ddf6d9c558eb25355fe89cc0fa
Vasilij Belosludcev <bupy765@gmail.com>
2017-04-29 21:38:25 +0500

    #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,29 +35,7 @@ module.exports = function(grunt) {
         },
         karma: {
             options: {
-                configFile: 'karma.conf.js',
-                files: [
-                    // depends
-                    'build/cropbox.css',
-                    'node_modules/chai/chai.js',
-                    'tests/lib/chai.js',
-                    'tests/lib/chai-equal-image.js',
-                    'node_modules/babel-polyfill/browser.js',               
-                    'build/cropbox.js',
-                    // hooks
-                    'tests/hooks/before.js',
-                    'tests/hooks/before-each.js',
-                    // tests
-                    'tests/methods/*.js',
-                    'tests/events/*.js',
-                    // assets
-                    {pattern: 'tests/assets/*.png', watched: false, included: false, served: true, nocache: false},
-                    // fixtures
-                    'tests/fixtures/body.html'
-                ],
-                proxies: {
-                    '/assets/': '/base/tests/assets/'
-                }
+                configFile: 'karma.conf.js'
             },
             dev: {},
             ci: {
--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,28 @@
 module.exports = function(config) {
     config.set({
         autoWatch: false,
+        files: [
+            // depends
+            'build/cropbox.css',
+            'node_modules/chai/chai.js',
+            'tests/lib/chai.js',
+            'tests/lib/chai-equal-image.js',
+            'node_modules/babel-polyfill/browser.js',
+            'build/cropbox.js',
+            // hooks
+            'tests/hooks/before.js',
+            'tests/hooks/before-each.js',
+            // tests
+            'tests/methods/*.js',
+            'tests/events/*.js',
+            // assets
+            {pattern: 'tests/assets/*.png', watched: false, included: false, served: true, nocache: false},
+            // fixtures
+            'tests/fixtures/body.html'
+        ],
+        proxies: {
+            '/assets/': '/base/tests/assets/'
+        },
         frameworks: ['mocha'],
         singleRun: true,
         browsers: ['PhantomJS'],
